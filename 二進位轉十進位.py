c798e24d626d00eaed83cc32c83537f2257f9101
Eric Yang <jie9481@gmail.com>
2024-03-05 17:15:39 +0800

    寫完

--- 二進位轉十進位.py
+++ 二進位轉十進位.py
@@ -1,14 +1,17 @@
 print("請輸入二進位的數字", end = "")
-num = int(input())
+num = input() 
 numlist=[]
 numout=0
-numlist = [int(a) for a in str(num)]
+integer_part, decimal_part = num.split(".") # 用小數點分開整數和小數
+numlist = [int(a) for a in str(integer_part)]
 power=len(numlist)-1
 for digit in numlist:
     numout += digit * (2 ** power)
     power -= 1
-    
+if decimal_part:
+    numlist = [int(a) for a in str(decimal_part)]
+    power=-1
+    for digit in numlist:
+        numout += digit * (2 ** power)
+        power -= 1
 print(numout)
-
-
-
